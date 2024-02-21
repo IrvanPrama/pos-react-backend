36f5a82053b69025edffd82ce9c6c3b35a6d9e79
IrvanPrama <bluxfusion@gmail.com>
2024-02-21 19:41:20 +0800

    setting transaction

--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -11,7 +11,6 @@ import {
   destroyData,
   handleUpdateStatus,
   filterTransactions,
-  // Variable untuk mengambil total transaksi
   getTotalTransactions,
 } from "../controllers/TransactionControllers.js";
 
