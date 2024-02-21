6c8252f5a23a45ae9ce3a649ee2c7b2b6e7b28d1
IrvanPrama <bluxfusion@gmail.com>
2024-02-21 19:40:19 +0800

    setting name controller

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,12 +1,12 @@
 // routers/authRouter.js
 import express from "express";
-import { login, logout, Me } from "../controllers/AuthControllers.js";
+import { Login, Logout, Me } from "../controllers/AuthControllers.js";
 
 const router = express.Router();
 // Step 14, auth login
 // Step 15, ada di AuthController.js
 router.get("/me", Me);
-router.post("/login", login);
-router.delete("/logout", logout);
+router.post("/login", Login);
+router.delete("/logout", Logout);
 
 export default router;
